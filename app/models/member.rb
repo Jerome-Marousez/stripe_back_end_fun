263a4e54f81be4ba16e6b86ae583dd0ee6f1ea81
Jerome Marousez <c-jmarousez@springbig.com>
2023-03-20 13:29:42 +0000

    added model validation

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,6 @@ class Member < ApplicationRecord
 	has_one :subscription
 	has_many :subscription_plan, through: :subscriptions
 
+	validates :username, presence: true
+
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,4 +2,7 @@ class Subscription < ApplicationRecord
 	belongs_to :member, dependent: :destroy
 	belongs_to :subscription_plan, dependent: :destroy
 
+	validates :member, presence: true
+	validates :subscription_plan, presence: true
+
 end
